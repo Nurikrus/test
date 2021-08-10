ab085be48c9e3dd23ac80bbce23af4f13109d75c
Ruslan <n_l_r@mail.ru>
2021-08-10 18:05:03 +0500

    Тестовое задание

--- test1/js/main.js
+++ test1/js/main.js
@@ -1,42 +1,55 @@
 'use strict';
 
-let btn = document.querySelector('.button');
-let numid = 1;
-let d = [
-    { className: 'div_one', id: 'one' },
-    { className: 'div_two', id: 'two' },
-];
-let p = [
-    { className: 'p1', id: 'p1_text' },
-    { className: 'p2', id: 'p1_text' },
-];
-
-let t = {
-    className: 'table1',
-    id: 'table1',
-};
-
-btn.addEventListener('click', createcoment);
-
-function createcoment() {
-    let insertComment = document.querySelector('.gridwrap');
-    if (numid == 1) {
-        numid = 0;
-        insertComment.insertAdjacentHTML('beforeend', render());
-    } else {
-        numid = 1;
-        insertComment.insertAdjacentHTML('beforeend', render());
+const app = new Vue({
+    el: '#app',
+    data: {
+        user: {
+            userName: '',
+            userMail: '',
+            userComent: '',
+        },
+        numid: 1,
+        p: [
+            { className: 'p1', id: 'p1_text' },
+            { className: 'p2', id: 'p1_text' },
+        ],
+        d: [
+            { className: 'div_one', id: 'one' },
+            { className: 'div_two', id: 'two' },
+        ],
+        t: {
+            className: 'table1',
+            id: 'table1',
+        },
+    },
+    methods: {
+        onSubmit() {
+            let insertComment = document.querySelector('.gridwrap');
+            if (this.numid == 1) {
+                this.numid = 0;
+                insertComment.insertAdjacentHTML('beforeend', this.render());
+                this.user.userName = '';
+                this.user.userMail = '';
+                this.user.userComent = '';
+            } else {
+                this.numid = 1;
+                insertComment.insertAdjacentHTML('beforeend', this.render());
+                this.user.userName = '';
+                this.user.userMail = '';
+                this.user.userComent = '';
+            }
+        },
+        render() {
+            return `<div class="${this.t.className} id="${this.t.id}">
+                        <div class="${this.d[this.numid].className}" id="${this.d[this.numid].id}"> 
+                        <h1>${this.user.userName}</h1>
+                        <p class="${this.p[this.numid].className}" id="${this.p[this.numid].id}">
+                            <b>${this.user.userMail}</b> <br><br>
+                            ${this.user.userComent}
+                        </p>
+                        </div>
+                    </div>`
+        }
     }
-    function render() {
-        return `<div class="${t.className} id="${t.id}">
-                    <div class="${d[numid].className}" id="${d[numid].id}"> 
-                    <h1>${nameUser.value}</h1>
-                    <p class="${p[numid].className}" id="${p[numid].id}">
-                        <b>${email.value}</b> <br><br>
-                        ${txtarea.value}
-                    </p>
-                    </div>
-                </div>`
-    }
-}
 
+})
